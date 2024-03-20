771e9a287c7366e3b6d58d01ad5b176a86b6c0fd
greensd4 <greenspan1993@gmail.com>
2024-03-20 09:56:40 +0200

    change main

--- cmd/main.go
+++ cmd/main.go
@@ -10,17 +10,17 @@ const defaultImage2 = "golang:1.21.5-alpine3.18"
 
 func main() {
 
-	//scanPath := "./test_files/withDockerInZip.zip"
+	scanPath := "./test_files/withDockerInZip.zip"
 	//scanPath := "./test_files/withHelmInZip.zip"
 	//scanPath := "./test_files/withDockerInTar.tar.gz"
-	scanPath := "/Users/danielgreenspan/GolandProjects/containers-resolver/empty"
+	//scanPath := "path-to-local-dir"
 
 	resultPath := "./test_files"
 	//resultPath := "./test_files/helm-results"
 	//resultPath := "./test_files/tar-results"
 	//resultPath := "./test_files/dir-results"
 
-	err := containersResolver.Resolve(scanPath, resultPath, []string{defaultImage}, true)
+	err := containersResolver.Resolve(scanPath, resultPath, []string{defaultImage, defaultImage2}, true)
 	if err != nil {
 		log.Println("Could not resolve containers", err)
 	}
