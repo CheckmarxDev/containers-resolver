35724034940b48fa4bda636784c4aaa8057f256a
davidkesoshvili <103584072+davidkesoshvili@users.noreply.github.com>
2025-01-02 16:40:23 +0200

    use distro codename (AST-76766) (#36)
    * use codename
    
    * tests

--- internal/syftExtractor/syft_extractor_test.go
+++ internal/syftExtractor/syft_extractor_test.go
@@ -1,9 +1,10 @@
 package syftExtractor
 
 import (
+	"testing"
+
 	"github.com/CheckmarxDev/containers-resolver/internal/logger"
 	"github.com/CheckmarxDev/containers-resolver/internal/types"
-	"testing"
 )
 
 func TestSyftExtractor(t *testing.T) {
@@ -26,7 +27,7 @@ func TestSyftExtractor(t *testing.T) {
 			Packages       int
 			ImageLocations int
 		}{
-			"rabbitmq:3":               {Layers: 9, Packages: 107, ImageLocations: 1},
+			"rabbitmq:3":               {Layers: 9, Packages: 100, ImageLocations: 1},
 			"golang:1.21.5-alpine3.18": {Layers: 4, Packages: 38, ImageLocations: 1},
 		}
 
@@ -50,7 +51,7 @@ func TestSyftExtractor(t *testing.T) {
 			Packages       int
 			ImageLocations int
 		}{
-			"rabbitmq:3": {Layers: 9, Packages: 107, ImageLocations: 2},
+			"rabbitmq:3": {Layers: 9, Packages: 100, ImageLocations: 2},
 		}
 
 		checkResults(t, resolutions, expectedValues)
@@ -112,7 +113,7 @@ func TestSyftExtractor(t *testing.T) {
 			Packages       int
 			ImageLocations int
 		}{
-			"rabbitmq:3": {Layers: 9, Packages: 107, ImageLocations: 1},
+			"rabbitmq:3": {Layers: 9, Packages: 100, ImageLocations: 1},
 		}
 
 		checkResults(t, resolutions, expectedValues)
